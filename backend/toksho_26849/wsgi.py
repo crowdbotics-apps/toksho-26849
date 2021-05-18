4e964daead97d5dbf676368fa31606155797446d
Crowdbotics <team@crowdbotics.com>
2021-05-18 06:34:08 +0000

    Initial commit.

---
+++ backend/toksho_26849/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for toksho_26849 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'toksho_26849.settings')
+
+application = get_wsgi_application()
